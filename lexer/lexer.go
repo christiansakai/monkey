e8496db9a9099e369ffc65d022596c8f1ad68410
Christian Sakai <christianmsakai@gmail.com>
2020-01-06 03:16:10 -0500

    Parse string literal

--- lexer/lexer.go
+++ lexer/lexer.go
@@ -101,6 +101,10 @@ func (l *Lexer) NextToken() token.Token {
 	case '}':
 		tok = newToken(token.RBRACE, l.ch)
 
+  case '"':
+    tok.Type = token.STRING
+    tok.Literal = l.readString()
+
 	case 0:
 		tok.Literal = ""
 		tok.Type = token.EOF
@@ -158,6 +162,19 @@ func (l *Lexer) peekChar() byte {
 	} else {
 		return l.input[l.readPosition]
 	}
+}
+
+func (l *Lexer) readString() string {
+  position := l.position + 1
+
+  for {
+    l.readChar()
+    if l.ch == '"' || l.ch == 0 {
+      break
+    }
+  }
+
+  return l.input[position:l.position]
 }
 
 func newToken(tokenType token.TokenType, ch byte) token.Token {
--- token/token.go
+++ token/token.go
@@ -14,6 +14,7 @@ const (
 	// Identifiers + literals
 	IDENT = "IDENT"
 	INT   = "INT"
+  STRING = "STRING"
 
 	// Operators
 	ASSIGN   = "="

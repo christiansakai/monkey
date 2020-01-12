eb6bee2b95e12c840559a59dbba3df201a12c893
Christian Sakai <christianmsakai@gmail.com>
2020-01-11 21:35:38 -0500

    Add conditionals VM

--- vm/vm_test.go
+++ vm/vm_test.go
@@ -36,7 +36,7 @@ func TestIntegerArithmetic(t *testing.T) {
 		{"(5 + 10 * 2 + 15 / 3) * 2 + -10", 50},
 	}
 
-	runVmTest(t, tests)
+	runVmTests(t, tests)
 }
 
 func TestBooleanExpresssions(t *testing.T) {
@@ -66,12 +66,30 @@ func TestBooleanExpresssions(t *testing.T) {
 		{"!!true", true},
 		{"!!false", false},
 		{"!!5", true},
+		{"!(if (false) { 5; })", true},
+		{"if ((if (false) { 10 })) { 10 } else { 20 }", 20},
 	}
 
-	runVmTest(t, tests)
+	runVmTests(t, tests)
 }
 
-func runVmTest(t *testing.T, tests []vmTestCase) {
+func TestConditionals(t *testing.T) {
+	tests := []vmTestCase{
+		{"if (true) { 10 }", 10},
+		{"if (true) { 10 } else { 20 }", 10},
+		{"if (false) { 10 } else { 20 } ", 20},
+		{"if (1) { 10 }", 10},
+		{"if (1 < 2) { 10 }", 10},
+		{"if (1 < 2) { 10 } else { 20 }", 10},
+		{"if (1 > 2) { 10 } else { 20 }", 20},
+		{"if (1 > 2) { 10 }", Null},
+		{"if (false) { 10 }", Null},
+	}
+
+	runVmTests(t, tests)
+}
+
+func runVmTests(t *testing.T, tests []vmTestCase) {
 	t.Helper()
 
 	for _, tt := range tests {
@@ -113,6 +131,11 @@ func testExpectedObject(
 		err := testBooleanObject(bool(expected), actual)
 		if err != nil {
 			t.Errorf("testBooleanObject failed:\n %s", err)
+		}
+
+	case *object.Null:
+		if actual != Null {
+			t.Errorf("object is not Null: %T (%+v)", actual, actual)
 		}
 	}
 }
